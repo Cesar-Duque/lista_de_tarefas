83843ae62f59ac1d8cc0ce1484c9794d0aa29822
Cesar-Duque <cduquebr@gmail.com>
2024-06-22 19:48:53 -0300

    SCSS atualizado

--- Frontend/todo-list/src/app/app-routing.module.ts
+++ Frontend/todo-list/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'add-task', component: TaskFormComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
